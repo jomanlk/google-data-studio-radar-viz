a62b23c4fb3b6fe6db506213ad43c562b15fa368
JohnP <john@rentman.nl>
2021-08-02 15:10:50 +0530

    Initial commit

---
+++ src/localMessage.js
@@ -0,0 +1,287 @@
+export const message = {
+  "tables": {
+    "DEFAULT": [
+      {
+        "dimID": [
+          "Rand Al'Thor"
+        ],
+        "metricID": [
+          99,
+          48,
+          88,
+          62
+        ]
+      },
+      {
+        "dimID": [
+          "Simone Sant"
+        ],
+        "metricID": [
+          87,
+          69,
+          69,
+          49
+        ]
+      },
+      {
+        "dimID": [
+          "Ariel Askos"
+        ],
+        "metricID": [
+          70,
+          78,
+          69,
+          99
+        ]
+      },
+      {
+        "dimID": [
+          "Pant Smith"
+        ],
+        "metricID": [
+          69,
+          78,
+          99,
+          48
+        ]
+      },
+      {
+        "dimID": [
+          "Babe Ruth"
+        ],
+        "metricID": [
+          49,
+          6,
+          74,
+          62
+        ]
+      },
+      {
+        "dimID": [
+          "Matt Smith"
+        ],
+        "metricID": [
+          48,
+          85,
+          41,
+          84
+        ]
+      }
+    ]
+  },
+  "fields": {
+    "dimID": [
+      {
+        "id": "qt_tv6n4kh6lc",
+        "name": "Names",
+        "type": "TEXT",
+        "concept": "DIMENSION"
+      }
+    ],
+    "metricID": [
+      {
+        "id": "qt_3e3u4jh6lc",
+        "name": "Biology",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      },
+      {
+        "id": "qt_w63u4jh6lc",
+        "name": "English",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      },
+      {
+        "id": "qt_x63u4jh6lc",
+        "name": "Mathematics",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      },
+      {
+        "id": "qt_y63u4jh6lc",
+        "name": "Chemistry",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
+  },
+  "style": {},
+  "theme": {
+    "themeFillColor": {
+      "color": "#ffffff",
+      "themeRef": {
+        "index": 0
+      }
+    },
+    "themeFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 1
+      }
+    },
+    "themeFontFamily": "Roboto",
+    "themeAccentFillColor": {
+      "color": "#e0e0e0",
+      "themeRef": {
+        "index": 2
+      }
+    },
+    "themeAccentFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 3
+      }
+    },
+    "themeAccentFontFamily": "Roboto",
+    "themeSeriesColor": [
+      {
+        "color": "#0072f0",
+        "seriesRef": {
+          "index": 0
+        },
+        "themeRef": {
+          "index": 4
+        }
+      },
+      {
+        "color": "#00b6cb",
+        "seriesRef": {
+          "index": 1
+        },
+        "themeRef": {
+          "index": 5
+        }
+      },
+      {
+        "color": "#f10096",
+        "seriesRef": {
+          "index": 2
+        },
+        "themeRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#f66d00",
+        "seriesRef": {
+          "index": 3
+        },
+        "themeRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ffa800",
+        "seriesRef": {
+          "index": 4
+        },
+        "themeRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#7cb342",
+        "seriesRef": {
+          "index": 5
+        },
+        "themeRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#5e35b1",
+        "seriesRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#03a9f4",
+        "seriesRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ec407a",
+        "seriesRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#ff7043",
+        "seriesRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#737373",
+        "seriesRef": {
+          "index": 10
+        }
+      },
+      {
+        "color": "#f15a60",
+        "seriesRef": {
+          "index": 11
+        }
+      },
+      {
+        "color": "#7ac36a",
+        "seriesRef": {
+          "index": 12
+        }
+      },
+      {
+        "color": "#5a9bd4",
+        "seriesRef": {
+          "index": 13
+        }
+      },
+      {
+        "color": "#faa75a",
+        "seriesRef": {
+          "index": 14
+        }
+      },
+      {
+        "color": "#9e67ab",
+        "seriesRef": {
+          "index": 15
+        }
+      },
+      {
+        "color": "#ce7058",
+        "seriesRef": {
+          "index": 16
+        }
+      },
+      {
+        "color": "#d77fb3",
+        "seriesRef": {
+          "index": 17
+        }
+      },
+      {
+        "color": "#81d4fa",
+        "seriesRef": {
+          "index": 18
+        }
+      },
+      {
+        "color": "#f48fb1",
+        "seriesRef": {
+          "index": 19
+        }
+      }
+    ],
+    "themeIncreaseColor": {
+      "color": "#388e3c"
+    },
+    "themeDecreaseColor": {
+      "color": "#f44336"
+    },
+    "themeGridColor": {
+      "color": "#d1d1d1"
+    }
+  },
+  "interactions": {}
+};
